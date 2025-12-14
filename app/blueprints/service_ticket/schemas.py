5a4f2fb0c0e670b737754a5006e6d7cc8cd772f1
Dhanushka <dhanushkamcr@gmail.com>
2025-12-14 02:09:15 -0600

    Get all assigned mechanics while retrieving service tickets data

--- app/blueprints/service_ticket/schemas.py
+++ app/blueprints/service_ticket/schemas.py
@@ -1,10 +1,14 @@
 from app.extensions import ma
 from app.models import ServiceTicket
+from marshmallow import fields
 
 class TicketSchema(ma.SQLAlchemyAutoSchema):
+    mechanics = fields.Nested('MechanicSchema', many=True)
+    
     class Meta:
         model = ServiceTicket
         include_fk = True
+        include_relationships = True
     
 ticket_schema = TicketSchema()
 tickets_schema = TicketSchema(many=True)
