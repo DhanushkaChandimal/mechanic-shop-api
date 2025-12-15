2628949d4f9b5aa00f2767d5b00c5062f277b061
Dhanushka <dhanushkamcr@gmail.com>
2025-12-15 05:57:53 -0600

    Created token required decorator

--- app/utils/util.py
+++ app/utils/util.py
@@ -1,5 +1,8 @@
+import jose
 from jose import jwt
 from datetime import datetime, timedelta, timezone
+from functools import wraps
+from flask import request, jsonify
 
 SECRET_KEY = "super secret, secret key"
 
@@ -12,3 +15,28 @@ def encode_token(user_id): #using unique pieces of info to make our tokens user 
 
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
     return token
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # Look for the token in the Authorization header
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split(" ")[1]
+        
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 400
+
+        try:
+            # Decode the token
+            data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            user_id = data['sub']  # Fetch the user ID
+            
+        except jose.exceptions.ExpiredSignatureError:
+             return jsonify({'message': 'Token has expired!'}), 400
+        except jose.exceptions.JWTError:
+             return jsonify({'message': 'Invalid token!'}), 400
+
+        return f(user_id, *args, **kwargs)
+
+    return decorated
