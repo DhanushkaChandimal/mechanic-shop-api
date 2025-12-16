f4495cadff96ab0a60f5cca629e8b6f85085d578
Dhanushka <dhanushkamcr@gmail.com>
2025-12-16 15:43:42 -0600

    Applied pagination to get customers route

--- app/blueprints/customers/routes.py
+++ app/blueprints/customers/routes.py
@@ -53,12 +53,18 @@ def create_customer():
 
 #GET ALL CUSTOMERS
 @customers_bp.route("/", methods=['GET'])
-@cache.cached(timeout=60)
 def get_customers():
-    query = select(Customer)
-    customers = db.session.execute(query).scalars().all()
+    try:
+        page = int(request.args.get('page'))
+        per_page = int(request.args.get('per_page'))
+        query = select(Customer)
+        customers = db.paginate(query, page=page, per_page=per_page)
+        return customers_schema.jsonify(customers), 200
+    except:
+        query = select(Customer)
+        customers = db.session.execute(query).scalars().all()
 
-    return customers_schema.jsonify(customers)
+        return customers_schema.jsonify(customers), 200
 
 #GET SPECIFIC CUSTOMER
 @customers_bp.route("/<int:customer_id>", methods=['GET'])
